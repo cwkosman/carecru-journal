440033bd1aeb6ceee9f432ebbf257584344a1f94
Chuck Kosman <cwkosman@gmail.com>
2017-04-02 15:39:13 -0700

    Basic webpack, dev-server, react setup

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,17 @@ const path = require('path');
 
 module.exports = {
   devtool: 'source-map',
-  entry: path.resolve(__dirname, 'src/index.jsx'),
+  entry: path.resolve(__dirname, 'src/app.jsx'),
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
   module: {
     rules: [{
-      test: /\.jsx$/,
+      test: /\.jsx?$/,
       loader: 'babel-loader',
-      include: [path.resolve(__dirname, 'src')]
+      include: [path.resolve(__dirname, 'src')],
+      options: { presets: ['es2015', 'stage-0', 'react'] }
     }]
   }
 };
